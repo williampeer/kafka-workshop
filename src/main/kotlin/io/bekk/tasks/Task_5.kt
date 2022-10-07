aaceedce1e5ba35ca9ce2681d83722ae17153e5d
William <williapb@gmail.com>
2022-10-07 09:29:35 +0200

    Add task suggestions 5 and 6

---
+++ src/main/kotlin/io/bekk/tasks/Task_5.kt
@@ -0,0 +1,6 @@
+package io.bekk.tasks
+
+class Task_5 {
+
+    // Multiple consumers for topic. Notice that they're each assigned a sub-set of the partitions for the topic.
+}
---
+++ src/main/kotlin/io/bekk/tasks/Task_6.kt
@@ -0,0 +1,7 @@
+package io.bekk.tasks
+
+class Task_6 {
+
+    // Create a topic with log-compaction, and produce messages with a shared key. What happens when log compaction is
+    //  run?
+}
