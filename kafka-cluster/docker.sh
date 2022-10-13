e8916c2c4efe35e012a39f20ac63456e15e38442
William <williapb@gmail.com>
2022-10-13 19:47:18 +0200

    Setup docker-image for kafka-cluster

---
+++ kafka-cluster/docker.sh
@@ -0,0 +1,6 @@
+# Shell-script
+
+curl --silent --output docker-compose.yml \
+  https://raw.githubusercontent.com/confluentinc/cp-all-in-one/7.2.2-post/cp-all-in-one/docker-compose.yml
+
+docker-compose up -d
--- src/main/kotlin/io/bekk/tasks/Task_1.kt
+++ src/main/kotlin/io/bekk/tasks/Task_1.kt
@@ -1,7 +1,29 @@
 package io.bekk.tasks
 
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import java.time.Duration
+
 class Task_1 {
 
     // Consume a message from the topic "hello-world"
+    fun consumeMessageFromKafkaTopic() {
+        val topicName = "hello-world"
+        val consumer = KafkaConsumer<String, String>(
+            mapOf(
+                ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to "false",
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to "org.apache.kafka.common.serialization.StringDeserializer",
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to "org.apache.kafka.common.serialization.StringDeserializer",
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",  // "<replace-me>"
+            )
+        )
 
+        consumer.subscribe(listOf(topicName))
+
+        val consumerRecords = consumer.poll(Duration.ofSeconds(2))
+        consumerRecords.forEach { consumerRecord ->
+            println("Record: topic: ${consumerRecord.topic()}, offset:${consumerRecord.offset()}")
+            println("Record value: ${consumerRecord.value()}")
+        }
+    }
 }
