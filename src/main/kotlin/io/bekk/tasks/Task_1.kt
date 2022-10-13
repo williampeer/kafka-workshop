1da871e924f357772af0ab5716bff21346927a2c
William <williapb@gmail.com>
2022-10-13 19:47:29 +0200

    Solve Task 1

--- src/main/kotlin/io/bekk/tasks/Task_1.kt
+++ src/main/kotlin/io/bekk/tasks/Task_1.kt
@@ -4,26 +4,26 @@ import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import java.time.Duration
 
-class Task_1 {
+class Task_1
+// Consume a message from the topic "hello-world"
 
-    // Consume a message from the topic "hello-world"
-    fun consumeMessageFromKafkaTopic() {
-        val topicName = "hello-world"
-        val consumer = KafkaConsumer<String, String>(
-            mapOf(
-                ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to "false",
-                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to "org.apache.kafka.common.serialization.StringDeserializer",
-                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to "org.apache.kafka.common.serialization.StringDeserializer",
-                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",  // "<replace-me>"
-            )
+fun main() {
+    val topicName = "hello-world"
+    val consumer = KafkaConsumer<String, String>(
+        mapOf(
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to "org.apache.kafka.common.serialization.StringDeserializer",
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to "org.apache.kafka.common.serialization.StringDeserializer",
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",  // "<replace-me>"
+            ConsumerConfig.GROUP_ID_CONFIG to "my-consumer-group",
+            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "earliest",
         )
+    )
 
-        consumer.subscribe(listOf(topicName))
+    consumer.subscribe(listOf(topicName))
 
-        val consumerRecords = consumer.poll(Duration.ofSeconds(2))
-        consumerRecords.forEach { consumerRecord ->
-            println("Record: topic: ${consumerRecord.topic()}, offset:${consumerRecord.offset()}")
-            println("Record value: ${consumerRecord.value()}")
-        }
+    val consumerRecords = consumer.poll(Duration.ofMillis(500L))
+    consumerRecords.forEach { consumerRecord ->
+        println("Record: topic: ${consumerRecord.topic()}, offset:${consumerRecord.offset()}")
+        println("Record value: ${consumerRecord.value()}")
     }
 }
