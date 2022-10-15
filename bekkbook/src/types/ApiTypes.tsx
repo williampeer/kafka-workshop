4c665ea81c2009647f515f5838491abf6e7bf82c
William <williapb@gmail.com>
2022-10-15 09:06:53 +0200

    Add API types

---
+++ bekkbook/src/types/ApiTypes.tsx
@@ -0,0 +1,12 @@
+
+declare namespace Model {
+    namespace FeedEvent {
+        export interface Message
+        {
+            topicName: string;
+            message: string;
+        }
+    }
+}
+
+export {};
