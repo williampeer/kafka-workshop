f44a2e2226864ccfa9909ea93d87d4bfa6413997
William <williapb@gmail.com>
2022-10-16 16:42:13 +0200

    Fix Avro-schema, and solve task 4 extras

--- src/main/kotlin/io/bekk/tasks/Task_4.kt
+++ src/main/kotlin/io/bekk/tasks/Task_4.kt
@@ -2,6 +2,7 @@ package io.bekk.tasks
 
 import io.bekk.repository.getBareBonesConsumer
 import java.time.Duration
+import java.util.*
 
 class Task_4
 
@@ -10,15 +11,32 @@ class Task_4
 //  Notice that they're each assigned a sub-set of the partitions for the topic, and that this allows for
 //  horizontal scalability not only broker/server- but also client-side.
 fun main() {
-    val consumer1 = getBareBonesConsumer(groupId = "quick-readers-association")
-    val consumer2 = getBareBonesConsumer(groupId = "quick-readers-association")
-    val consumer3 = getBareBonesConsumer(groupId = "quick-readers-association")
+    val uniqueConsumerGroup = "quick-readers-association-${UUID.randomUUID()}"
+    val consumer1 = getBareBonesConsumer(groupId = uniqueConsumerGroup, offsetConfig = "earliest")
+    val consumer2 = getBareBonesConsumer(groupId = uniqueConsumerGroup, offsetConfig = "earliest")
+    val consumer3 = getBareBonesConsumer(groupId = uniqueConsumerGroup, offsetConfig = "earliest")
     val consumers = listOf(consumer1, consumer2, consumer3)
-    consumers.forEach { it.subscribe(listOf(topicName)) }
+
+    consumers.forEach { it.subscribe(listOf(topicName)) }  // partition-balancing
 
     consumers.forEachIndexed{ cIdx, consumer ->
         println("\nPolling records for consumer #$cIdx..")
-        val consumerRecords = consumer.poll(Duration.ofMillis(100))
+        val consumerRecords = consumer.poll(Duration.ofMillis(500))
+        println("==================== Consumer records ====================")
+        consumerRecords.forEach { consumerRecord ->
+            println("Record: topic: ${consumerRecord.topic()}, offset:${consumerRecord.offset()}")
+            println("Record value: ${consumerRecord.value()}")
+        }
+    }
+
+    // Optional: Re-use an already-existing consumer-group, such as "quick-readers-association", and read all messages
+    //  Hint: Even though we started reading from offset 0, the current value will be that of the last consumed message
+    //      for each partition..
+    consumers.forEachIndexed{ cIdx, consumer ->
+        println("\nPolling records for consumer #$cIdx..")
+        consumer.seekToBeginning(consumer.assignment())
+        val consumerRecords = consumer.poll(Duration.ofMillis(500))
+        println("==================== Consumer records ====================")
         consumerRecords.forEach { consumerRecord ->
             println("Record: topic: ${consumerRecord.topic()}, offset:${consumerRecord.offset()}")
             println("Record value: ${consumerRecord.value()}")
