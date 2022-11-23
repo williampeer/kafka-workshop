fc5a1a705b8a8a850f7bc44e67c6e83701330d7c
Brynjulv Brynjulvsen <brynjulv@bekk.no>
2022-11-23 12:43:37 +0100

    Adjusted wording for task_11

--- src/exercises/kotlin/tasks/Task_11.kt
+++ src/exercises/kotlin/tasks/Task_11.kt
@@ -2,4 +2,4 @@ package tasks
 
 // Task_11
 // Improving upon the spring boot app from Task_10, consider what will happen
-// if processing of a message fails. Create handling for this.
+// if processing of a message fails. Improve handling of this.
