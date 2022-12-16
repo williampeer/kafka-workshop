c015009c2a46f321eb54e1087bb0dc147adbac2a
William <williapb@gmail.com>
2022-12-16 09:20:11 +0100

    Update Solution 2 in python

--- src/exercises/python/suggested_solutions/Solution_2.py
+++ src/exercises/python/suggested_solutions/Solution_2.py
@@ -1,17 +1,16 @@
-from kafka import KafkaConsumer
 from uuid import uuid1
 
+from kafka import KafkaConsumer
 
 bootstrap_server = 'kafka-workshop-001-kafka-workshop.aivencloud.com:13816'
 consumer = KafkaConsumer(bootstrap_servers=bootstrap_server, security_protocol='SASL_SSL',
                          sasl_mechanism='SCRAM-SHA-256', sasl_plain_username='workshop',
-                         sasl_plain_password='<change-me>',
+                         sasl_plain_password='AVNS_TKo_VY74K_CVc2wTHR0',
                          group_id=str(uuid1()), auto_offset_reset='earliest')
 
 # Task_2
-# TODO: Consume a message from the topic "hello-world"
 consumer.subscribe(["hello-world"])
-sut = consumer.poll(timeout_ms=10000)
-print(sut)
-for record in sut:
-    print(record)
+while 1:
+    records_from_broker = consumer.poll(timeout_ms=3000)
+    for message in records_from_broker.values():
+        print(message)
